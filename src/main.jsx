088c52711356c5dc978ecced7e482b909bdc3b10
pdftw2htgt-glitch <joveniauxmathis20@gmail.com>
2026-03-06 23:21:50 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Devia from '../devia.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Devia />
+  </React.StrictMode>
+)
